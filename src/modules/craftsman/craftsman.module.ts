4a0247e1c636e1dcb62cb5ba19585a3c84db3f7d
Mohamed Affes <med123011@gmail.com>
2025-11-17 18:26:57 +0100

    feat(craftsman): add UploadModule to craftsman module imports

--- src/modules/craftsman/craftsman.module.ts
+++ src/modules/craftsman/craftsman.module.ts
@@ -4,9 +4,10 @@ import { Craftsman } from '../craftsman/craftsman.entity';
 import { CraftsmanService } from './craftsman.service';
 import { CraftsmanController } from './craftsman.controller';
 import { User } from 'src/modules/user/user.entity';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Craftsman, User])],
+  imports: [TypeOrmModule.forFeature([Craftsman, User]), UploadModule],
   providers: [CraftsmanService],
   controllers: [CraftsmanController],
   exports: [CraftsmanService],
