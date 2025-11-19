e4abbfa989d9a7360194792feb0d7e7adc7e6c91
Mohamed Affes <med123011@gmail.com>
2025-11-19 19:46:06 +0100

    feat: enhance product filtering and display features with dynamic updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,22 @@ import { Component, signal } from '@angular/core';
 import { Header } from './shared/components/header/header';
 import { SearchFiltersBar } from './features/homepage/components/search-filters-bar/search-filters-bar';
 import { SearchResultsTab } from './features/homepage/components/search-results-tab/search-results-tab';
-import { FollowingExplore } from './features/homepage/components/following-explore/following-explore';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductFilters } from './features/homepage/components/search-filters-bar/product-filters.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Header,SearchFiltersBar, FollowingExplore, SearchResultsTab, HttpClientModule],
+  imports: [Header,SearchFiltersBar, SearchResultsTab, HttpClientModule, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('ArtisanArt');
+
+  currentFilters: ProductFilters = {};
+
+  onFiltersChange(filters: ProductFilters) {
+    this.currentFilters = filters;
+  }
 }
--- src/app/features/homepage/components/search-results-tab/search-results-tab.ts
+++ src/app/features/homepage/components/search-results-tab/search-results-tab.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 import { ItemDisplay } from './item-display/item-display';
 import { CommonModule } from '@angular/common';
 import { Product, ProductService } from '../../../../core/services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductFilters } from '../search-filters-bar/product-filters.interface';
 
 @Component({
   selector: 'app-search-results-tab',
@@ -12,17 +13,34 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class SearchResultsTab {
   constructor(private productService: ProductService) {}
+  @Input() filters: ProductFilters = {};
 
-  products: Product[] = []; // List of products to display
+  products: Product[] = [];
+  isLoading = true;
 
-  // Fetch products on component initialization
   ngOnInit() {
-    this.productService.getProducts(1, 20).subscribe({
-      next: (res) => {
-        this.products = res.items;
-      },
-      error: (err) => console.error(err)
-    });
+    this.loadProducts();
   }
 
+  // Cette méthode est appelée chaque fois que @Input() change
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['filters'] && !changes['filters'].firstChange) {
+      this.loadProducts();
+    }
+  }
+
+  loadProducts() {
+    this.isLoading = true;
+
+    this.productService.getProducts(1, 20, this.filters).subscribe({
+      next: (res) => {
+        this.products = res.items;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      },
+    });
+  }
 }
