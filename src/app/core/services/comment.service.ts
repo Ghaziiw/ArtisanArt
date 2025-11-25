6e7e3db8df3b08b3e0a3afbe6c75a4c25651cd02
mayssabenmrad <b.mrad.mayssa@gmail.com>
2025-11-25 08:21:24 +0100

    feat : implement comments service

---
+++ src/app/core/services/comment.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Comment } from './specific-product.service';
+import { User } from './specific-product.service';
+
+export interface createCommentDto{
+    productId: string;
+    content: string;
+    mark: number;
+}
+
+export interface CommentsResponse {
+  items: Comment[];
+  meta: {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+  };
+}
+
+@Injectable({providedIn: 'root'})
+export class CommentService{
+    private apiUrl = 'http://localhost:3000/comments';
+
+    constructor(private http: HttpClient) {}
+    getComments(
+        productId: string,
+        page: number =1,
+        limit : number = 20
+    ): Observable<CommentsResponse>{
+        let params = new HttpParams().set('page', page.toString()).set('limit', limit.toString());
+        return this.http.get<CommentsResponse>(`${this.apiUrl}/${productId}`, { params });
+    }
+
+    addComment(commentData: createCommentDto) : Observable<Comment>{
+        const formData = new FormData();
+        formData.append('productId', commentData.productId);
+        formData.append('content', commentData.content);
+        formData.append('mark', commentData.mark.toString());
+        return this.http.post<Comment>(`${this.apiUrl}`, formData, {
+              withCredentials: true,
+            });
+    }
+
+
+
+}
