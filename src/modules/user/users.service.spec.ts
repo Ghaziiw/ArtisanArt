a6e46c51521abe3c5e273964208797e3729f5589
mayssabenmrad <b.mrad.mayssa@gmail.com>
2025-11-30 19:51:47 +0100

    test(user): update user service test

--- src/modules/user/users.service.spec.ts
+++ src/modules/user/users.service.spec.ts
@@ -19,9 +19,8 @@ const mockQueryBuilder: any = {
   limit: jest.fn().mockReturnThis(),
   offset: jest.fn().mockReturnThis(),
   cache: jest.fn().mockReturnThis(),
-  clone: jest.fn().mockImplementation(function () {
-    return this; // retourne le même objet pour que paginate fonctionne
-  }),
+  clone: jest.fn().mockReturnThis(),
+
   getMany: jest.fn(),
   getCount: jest.fn(),
 };
@@ -82,11 +81,7 @@ describe('UserService', () => {
         },
       });
 
-      const filters = {
-        email: 'mayssa@example.com',
-        name: 'John Doe',
-      };
-
+      const filters = { email: 'mayssa@example.com', name: 'John Doe' };
       const result = await service.findAll(1, 10, filters);
 
       expect(result.items.length).toBe(1);
@@ -106,7 +101,6 @@ describe('UserService', () => {
       });
 
       const result = await service.findAll(1, 10, {});
-
       expect(result.items).toEqual([]);
       expect(paginate).toHaveBeenCalled();
     });
@@ -115,7 +109,6 @@ describe('UserService', () => {
   describe('findOne', () => {
     it('should return a user if found', async () => {
       const mockUser = { id: '1', name: 'Mayssa', email: 'mayssa@example.com' };
-
       mockUserRepository.findOne.mockResolvedValue(mockUser);
 
       const result = await service.findOne('1');
@@ -127,25 +120,21 @@ describe('UserService', () => {
     });
 
     it('should throw NotFoundException if user not found', async () => {
-
       mockUserRepository.findOne.mockResolvedValue(null);
-
       await expect(service.findOne('1')).rejects.toThrow('User not found');
+    });
   });
-});
-
 
   describe('delete', () => {
     it('should delete a user without image', async () => {
       const userId = '1';
       const mockUser = { id: userId, image: null };
-
       mockUserRepository.findOne.mockResolvedValue(mockUser);
       mockUserRepository.delete.mockResolvedValue({
         affected: 1,
       } as DeleteResult);
 
-      const result = await service.deleteUserAdmin(userId);
+      const result = await service.deleteUser(userId);
 
       expect(mockUserRepository.findOne).toHaveBeenCalledWith({
         where: { id: userId },
@@ -158,14 +147,13 @@ describe('UserService', () => {
     it('should delete a user with image', async () => {
       const userId = '2';
       const mockUser = { id: userId, image: 'profile.jpg' };
-
       mockUserRepository.findOne.mockResolvedValue(mockUser);
       mockUserRepository.delete.mockResolvedValue({
         affected: 1,
       } as DeleteResult);
       mockUploadService.deleteFile.mockResolvedValue(undefined);
 
-      const result = await service.deleteUserAdmin(userId);
+      const result = await service.deleteUser(userId);
 
       expect(mockUserRepository.findOne).toHaveBeenCalledWith({
         where: { id: userId },
@@ -179,76 +167,70 @@ describe('UserService', () => {
       const userId = '999';
       mockUserRepository.findOne.mockResolvedValue(undefined);
 
-      await expect(service.deleteUserAdmin(userId)).rejects.toThrow('User not found');
+      await expect(service.deleteUser(userId)).rejects.toThrow(
+        'User not found',
+      );
+    });
+  });
 
+  describe('update profile', () => {
+    it('should update user profile when data is valid', async () => {
+      const userId = '1';
+      const updatedData = { name: 'New', email: 'new@mail.com' };
+      const existingUser = { id: userId, name: 'Old', email: 'old@mail.com' };
+
+      mockUserRepository.findOne
+        .mockResolvedValueOnce(existingUser)
+        .mockResolvedValueOnce(null);
+      mockUserRepository.save.mockResolvedValue({
+        ...existingUser,
+        ...updatedData,
+      });
+
+      const result = await service.updateProfile(userId, updatedData);
+
+      expect(mockUserRepository.findOne).toHaveBeenNthCalledWith(1, {
+        where: { id: userId },
+      });
+      expect(mockUserRepository.findOne).toHaveBeenNthCalledWith(2, {
+        where: { email: updatedData.email },
+      });
+      expect(mockUserRepository.save).toHaveBeenCalledWith({
+        ...existingUser,
+        ...updatedData,
+      });
+      expect(result.name).toBe(updatedData.name);
+      expect(result.email).toBe(updatedData.email);
     });
 
-    describe('update profile', () => {
-      it('should update user profile when data is valid', async () => {
-        const userId = '1';
-        const updatedData = { name: 'New', email: 'new@mail.com' };
-        const existingUser = {
-          id: userId,
-          name: 'Old',
-          email: 'old@mail.com',
-        };
+    it('should throw BadRequestException if user not found', async () => {
+      const userId = '1';
+      const updatedData = { name: 'New Name', email: 'new@mail.com' };
+      mockUserRepository.findOne.mockResolvedValue(undefined);
 
-        mockUserRepository.findOne
-          .mockResolvedValueOnce(existingUser) //simulate searching for the user
-          .mockResolvedValueOnce(null); //simulate searching for the email
-        mockUserRepository.save.mockResolvedValue({
-          ...existingUser,
-          ...updatedData,
-        });
+      await expect(service.updateProfile(userId, updatedData)).rejects.toThrow(
+        BadRequestException,
+      );
+      expect(mockUserRepository.save).not.toHaveBeenCalled();
+    });
 
-        const result = await service.updateProfile(userId, updatedData);
+    it('should throw BadRequestException if email is already in use', async () => {
+      const userId = '1';
+      const updatedData = { email: 'existing@mail.com' };
+      const existingUser = {
+        id: userId,
+        name: 'Old Name',
+        email: 'old@mail.com',
+      };
 
-        expect(mockUserRepository.findOne).toHaveBeenNthCalledWith(1, {
-          where: { id: userId },
-        });
+      mockUserRepository.findOne
+        .mockResolvedValueOnce(existingUser)
+        .mockResolvedValueOnce({ id: '1', email: 'existing@mail.com' });
 
-        expect(mockUserRepository.findOne).toHaveBeenNthCalledWith(2, {
-          where: { email: updatedData.email },
-        });
-
-        expect(mockUserRepository.save).toHaveBeenCalledWith({
-          ...existingUser,
-          ...updatedData,
-        });
-        expect(result.name).toBe(updatedData.name);
-        expect(result.email).toBe(updatedData.email);
-      });
-
-      it('should throw BadRequestException if user not found', async () => {
-        const userId = '1';
-        const updatedData = { name: 'New Name', email: 'new@mail.com' };
-
-        mockUserRepository.findOne.mockResolvedValue(undefined);
-
-        await expect(
-          service.updateProfile(userId, updatedData),
-        ).rejects.toThrow(BadRequestException);
-        expect(mockUserRepository.save).not.toHaveBeenCalled();
-      });
-
-      it('should throw BadRequestException if email is already in use', async () => {
-        const userId = '1';
-        const updatedData = { email: 'existing@mail.com' };
-        const existingUser = {
-          id: userId,
-          name: 'Old Name',
-          email: 'old@mail.com',
-        };
-
-        mockUserRepository.findOne
-          .mockResolvedValueOnce(existingUser)
-          .mockResolvedValueOnce({ id: '1', email: 'existing@mail.com' });
-
-        await expect(
-          service.updateProfile(userId, updatedData),
-        ).rejects.toThrow(BadRequestException);
-        expect(mockUserRepository.save).not.toHaveBeenCalled();
-      });
+      await expect(service.updateProfile(userId, updatedData)).rejects.toThrow(
+        BadRequestException,
+      );
+      expect(mockUserRepository.save).not.toHaveBeenCalled();
     });
   });
 });
