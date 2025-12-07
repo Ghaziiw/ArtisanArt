d49c6cd54f5f32c25fca7c2bb4e747952439e373
Mohamed Affes <med123011@gmail.com>
2025-12-07 19:07:03 +0100

    refactor(productpage): improve layout and styling for craftsman and product info components

--- src/app/features/productpage/components/product-info/product-info.ts
+++ src/app/features/productpage/components/product-info/product-info.ts
@@ -10,22 +10,21 @@ import { Offer } from '../../../../core/services/offer.service';
   styleUrl: './product-info.css',
 })
 export class ProductInfo {
- @Input() product!: Product;
- @Input() offer!: Offer;
- @Output() quantitySelected = new EventEmitter<number>();
- @Output() addProductToCart = new EventEmitter<void>();
+  @Input() product!: Product;
+  @Input() offer!: Offer;
+  @Output() quantitySelected = new EventEmitter<number>();
+  @Output() addProductToCart = new EventEmitter<void>();
 
   beforeOffer: number = 0;
   withOffer: number = 0;
   economy: number = 0;
-  rating: number = 0;
   starsArray: number[] = [];
   quantity : number = 1;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.product) {
       this.computePrices(); //to calculate discount, and the price with the discount dynamically
-      this.computeRating(); //to have an adequate format for the rating
+      this.computeRating(); //to calculate average rating and stars array
     }
   }
 
@@ -33,30 +32,21 @@ export class ProductInfo {
     this.beforeOffer = this.product.price;
     if (this.offer && this.offer.percentage) {
       const discount = (this.beforeOffer * this.offer.percentage) / 100;
-      this.withOffer = this.beforeOffer - discount;
-      this.economy = discount;
+      this.withOffer = (this.beforeOffer - discount).toFixed(2) as unknown as number;
+      this.economy = discount.toFixed(2) as unknown as number;
     } else {
       this.withOffer = this.beforeOffer;
       this.economy = 0;
     }
   }
 
+  // compute average rating and stars array
   private computeRating() {
-    if (this.product.comments) {
-      const total = this.product.comments.reduce((sum, c) => sum + c.mark, 0);
-      this.rating = total / this.product.comments.length;
-      this.rating = Number(this.rating.toFixed(2));
-      const rounded = Math.round(this.rating);
-      this.starsArray = Array(rounded).fill(0);
-    } else {
-      this.rating = 0;
-      this.starsArray = [];
-      return;
-    }
+    this.starsArray = Math.round(this.product.avgRating) ? this.getStarsArray(this.product.avgRating) : [];
   }
 
   //for stars 
-   getStarsArray(rating: number): number[] {
+  getStarsArray(rating: number): number[] {
     const rounded = Math.round(rating);
     return Array(rounded).fill(0);
   }
